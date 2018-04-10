9a4d68a1037bbd557e82e3f1aaa48d60de82d414
Arkadiusz Bartoszko-Kotarba <arkadiusz.kotarba@logicims.com>
2018-04-10 18:01:06 +0200

    updated windows and linux consts

--- const.rb
+++ const.rb
@@ -10,11 +10,11 @@ class Const
 		PROMO_FIX_Y = 10
 	else
 		FONT = 'Arial'
-		PIECE_FIX_X = 0
-		PIECE_FIX_Y = 0
-		FOG_SIZE = 0
+		PIECE_FIX_X = 10
+		PIECE_FIX_Y = 10
+		FOG_SIZE = 45
 		FOG_FIX_X = 0
-		FOG_FIX_Y = 0
+		FOG_FIX_Y = -10
 		PROMO_FIX_X = 0
 		PROMO_FIX_Y = 0
 	end
